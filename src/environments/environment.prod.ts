97006b45f95ec18cd511ab2f2717160897618ea1
R P <divanshi.r.p@accenture.com>
2018-10-08 17:41:33 +0530

    Second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDj1KS3gQTZZKf7X0IQta_1BauevD-6jfU',
+    authDomain: 'oshop-51d3b.firebaseapp.com',
+    databaseURL: 'https://oshop-51d3b.firebaseio.com',
+    projectId: 'oshop-51d3b',
+    storageBucket: 'oshop-51d3b.appspot.com',
+    messagingSenderId: '836639943075'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDj1KS3gQTZZKf7X0IQta_1BauevD-6jfU',
+    authDomain: 'oshop-51d3b.firebaseapp.com',
+    databaseURL: 'https://oshop-51d3b.firebaseio.com',
+    projectId: 'oshop-51d3b',
+    storageBucket: 'oshop-51d3b.appspot.com',
+    messagingSenderId: '836639943075'
+  }
 };
 
 /*
